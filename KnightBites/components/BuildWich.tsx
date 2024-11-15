ea51f57b712b340b358a56cf3a3e86e0b69cc53f
Lillian McAboy <lillianmcaboy@dhcp97-241.calvin.edu>
2024-11-14 20:29:25 -0500

    Adding extra buttons to sandwich page

--- KnightBites/components/BuildWich.tsx
+++ KnightBites/components/BuildWich.tsx
@@ -10,11 +10,11 @@ import { Colors } from '@/constants/Colors';
 
 export default function buildSandwich({navigation}) {
 
-    const [ email, setEmail ] = useState("");
-    const [ ps1, setPs1 ] = useState("");
-    const [ ps2, setPs2 ] = useState("");
-    const [isFocused, setIsFocused] = useState(false);
-    const [isPasswordVisible, allowPasswordVisible] = useState(false);
+    const [breadChoice, setBreadChoice] = useState("");
+    const [cheeseChoice, setCheeseChoice] = useState("");
+    const [veggieChoice, setVeggieChoice] = useState("");
+    const [condimentChoice, setCondimentChoice] = useState("");
+    const [creationName, setCreationName] = useState("");
 
 
     return (
@@ -24,13 +24,40 @@ export default function buildSandwich({navigation}) {
         <Text>Build your personal UpperCrust order and share with creation with everyone!</Text>
 
         <Text>Step 1: Pick your bread</Text>
+
+        
         <Text>Step 2: Pick your protein</Text>
         <Text>Step 3: Pick your cheese</Text>
         <Text>Step 4: Pick your veggies</Text>
         <Text>Step 5: Pick your condiments</Text>
 
-        <TouchableOpacity >Submit My Creation</TouchableOpacity>
+        <TextInput
+        style={[styles.loginTextBar]}
+        value={creationName}
+        onChangeText={setCreationName} 
+        placeholder="Enter a name for your creation!"
+      />
+      <TouchableOpacity onPress={() => navigation.navigate("buildSandwichHomePage")} style = {UpperCrustStyles.submitButton}><Text style = {UpperCrustStyles.buttonText}>Submit Creation</Text></TouchableOpacity>
 
       </View>
     );
   };
+
+  const UpperCrustStyles = StyleSheet.create({
+
+    submitButton: {
+      borderRadius: 25,
+      borderColor: "black",
+      borderWidth: 2,
+      width: 150,
+      height: 60,
+      alignItems: "center", // Center horizontally
+      justifyContent: "center", // Center vertically
+    },
+    buttonText: {
+      textAlign: "center",
+      fontSize: 15,
+      color: "black",
+    },
+
+  });
