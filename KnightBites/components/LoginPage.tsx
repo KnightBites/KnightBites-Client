e6ffec541c6b219924c0ea2cd0340d21c2808a4c
CheetoLord <pbl4@calvin.edu>
2024-12-19 08:13:21 -0500

    various

--- KnightBites/components/LoginPage.tsx
+++ KnightBites/components/LoginPage.tsx
@@ -115,7 +115,7 @@ export default function LoginPage({ navigation }) {
         <Text style={styles.submitText}>Submit</Text>
       </TouchableOpacity>
       <TouchableOpacity onPress={() => navigation.navigate("registration")}><Text style={{ marginTop: 15, color: "blue" }}>Don't have an account? Register here.</Text></TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate("recovery")}><Text style={{ marginTop: 15, color: "blue" }}>Forgot your password? Recover account here.</Text></TouchableOpacity>
+      {/* <TouchableOpacity onPress={() => navigation.navigate("recovery")}><Text style={{ marginTop: 15, color: "blue" }}>Forgot your password? Recover account here.</Text></TouchableOpacity> */}
       {/*<TouchableOpacity onPress={() => navigation.navigate("buildSandwichHomePage")}><Text style={{ marginTop: 15, color: "blue" }}>View Uppercrust.</Text></TouchableOpacity>*/}
       {loading && <ActivityIndicator />}
     </View>
